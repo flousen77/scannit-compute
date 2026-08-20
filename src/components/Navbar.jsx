cde8a884fa47153b2a2e0ace51c2e6495b48b757
Flsn7 <floutscrap@gmail.com>
2026-08-21 01:59:08 +0200

    feat: node page ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar({ openModal }) {
         <div className="container">
           <a href="/" className="logo">
             <img src="https://imagedelivery.net/Ulul0QO-cXqPUi6uJcNN5g/a3924725-4e64-4885-0779-1aae85136500/public" alt="Scannit Logo" className="logo-img" />
-            <span>Scannit <span className="sub-brand"></span></span>
+            <span>Scannit</span>
           </a>
           <div className="nav-actions">
             <a href="/node" className="investor-link">
